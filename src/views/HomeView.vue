9725598c80689c92246a38f3650c933877fd89b3
Guillaume Charest <guillaume.charest@cds-snc.ca>
2022-02-11 13:51:48 -0500

    Update component with StatusFeed.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,20 @@
 <script setup lang="ts">
-import TheWelcome from "@/components/TheWelcome.vue";
+import { ref } from "vue";
+import StatusFeed from "@/components/StatusFeed.vue";
+import { useFeedsStore } from "@/stores/feeds";
+const feedsStore = useFeedsStore();
+const title = feedsStore.currentStatus;
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>Current status of 3rd party services</h1>
+  <ul class="card-deck">
+    <StatusFeed :title="title" :msg="feedsStore.getXML" />
+  </ul>
 </template>
+
+<style scoped>
+.card-deck {
+  padding-left: 0px !important;
+}
+</style>
