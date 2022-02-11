f91f8b6d72cfa5565ced822edd04b1e1051ac0d4
Guillaume Charest <guillaume.charest@cds-snc.ca>
2022-02-11 13:52:00 -0500

    Change layout to Bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,55 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "@/components/HelloWorld.vue";
 </script>
 
 <template>
   <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container-fluid">
+        <RouterLink to="/" class="navbar-brand">Status</RouterLink>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <RouterLink
+                to="/about"
+                class="nav-link active"
+                aria-current="page"
+                >About</RouterLink
+              >
+            </li>
+          </ul>
+          <form class="d-flex">
+            <input
+              class="form-control me-2"
+              type="Filter"
+              placeholder="Filter"
+              aria-label="Filter"
+            />
+            <button class="btn btn-outline-success" type="submit">
+              Filter
+            </button>
+          </form>
+        </div>
+      </div>
+    </nav>
   </header>
-
-  <RouterView />
+  <main class="bd-main order-1">
+    <RouterView class="container-xl" />
+  </main>
 </template>
 
-<style>
+<!-- <style>
 @import "@/assets/base.css";
 
 #app {
@@ -122,4 +145,4 @@ nav a:first-of-type {
     margin-top: 1rem;
   }
 }
-</style>
+</style> -->
