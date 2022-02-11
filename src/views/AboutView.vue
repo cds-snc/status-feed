580b6f7341fff2aace787d6aafe1de83d073d1c4
Guillaume Charest <guillaume.charest@cds-snc.ca>
2022-02-11 13:50:23 -0500

    Cleanup About view

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,10 +1,18 @@
+<script setup lang="ts">
+import StatusFeed from "@/components/StatusFeed.vue";
+</script>
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About</h1>
+    <p>
+      This micro-site lists the latest known status for 3rd party Software as a
+      service (SaaS) used in CDS.
+    </p>
+    <!-- <StatusFeed title="test" msg="msg"/> -->
   </div>
 </template>
 
-<style>
+<!-- <style>
 @media (min-width: 1024px) {
   .about {
     min-height: 100vh;
@@ -12,4 +20,4 @@
     align-items: center;
   }
 }
-</style>
+</style> -->
