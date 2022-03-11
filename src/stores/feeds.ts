f43865a8d338e465b364e60de2ca7982d418a614
Guillaume Charest <guillaume.charest@cds-snc.ca>
2022-03-11 13:37:17 -0500

    Add url pattern switch

--- src/stores/feeds.ts
+++ src/stores/feeds.ts
@@ -6,7 +6,14 @@ export const useFeedsStore = defineStore({
     feeds: [
       {
         name: "Trello",
-        url: "https://trello.status.atlassian.com/history.atom",
+        url: "https://trello.status.atlassian.com/histor2y.atom",
+        pattern: "statuspage",
+        lastUpdated: undefined,
+        status: "unknown",
+      },
+      {
+        name: "Random",
+        pattern: "random",
       },
     ],
   }),
@@ -14,17 +21,43 @@ export const useFeedsStore = defineStore({
     currentStatus(state) {
       return state.feeds[0].name;
     },
-    getXML(state) {
-      let xmlDoc = "";
-      fetch(state.feeds[0].url)
+  },
+  actions: {
+    getFeedStatus(url: string | undefined, pattern: string) {
+      switch (pattern) {
+        case "statuspage":
+          if (url !== undefined) return "<p>All services operational</p>";
+          return "<p>Error getting status</p>";
+        default:
+          return "<p>Status unknown</p>";
+      }
+    },
+    getXML(url: string) {
+      let xmlContent: string[] = [];
+      console.log(xmlContent);
+      fetch(url)
         .then((res) => res.text())
-        .then((text) => {
-          xmlDoc = text;
-          console.log(text);
+        .then((xml) => {
+          const parser = new DOMParser();
+          const doc = parser.parseFromString(xml, "text/xml");
+          xmlContent = Array.from(doc.getElementsByTagName("entry")).map(
+            (entryContent) => {
+              if (entryContent.textContent) {
+                return entryContent.textContent;
+              }
+              return "empty";
+            }
+          );
+          console.log(xmlContent);
+          // if (typeof xmlContent[0] === "string") {
+          //   return xmlContent[0];
+          // } else {
+          //   return "empty content";
+          // }
         })
         .catch((err) => console.log(err));
-      return xmlDoc;
+      console.log(xmlContent);
+      return xmlContent;
     },
   },
-  actions: {},
 });
